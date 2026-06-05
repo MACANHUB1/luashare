b2302b39f7cd4802eb0ed124826b0aca3ba09051
MACANHUB1 <xquiar@atomicmail.io>
2026-06-05 15:26:00 +0300

    Update [code].js

--- functions/api/script/[code].js
+++ functions/api/script/[code].js
@@ -1,15 +1,28 @@
+function json(data, status = 200) {
+  return new Response(JSON.stringify(data), {
+    status,
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+}
+
 export async function onRequestGet({ params, env }) {
+  if (!env.SCRIPTS) {
+    return json({ error: "KV SCRIPTS не подключен" }, 500)
+  }
+
   const code = params.code
 
   if (!/^\d{6}$/.test(code)) {
-    return Response.json({ error: "Неверный код" }, { status: 400 })
+    return json({ error: "Неверный код" }, 400)
   }
 
   const script = await env.SCRIPTS.get(`script:${code}`)
 
   if (!script) {
-    return Response.json({ error: "Скрипт не найден" }, { status: 404 })
+    return json({ error: "Скрипт не найден" }, 404)
   }
 
-  return Response.json({ script })
+  return json({ script })
 }
